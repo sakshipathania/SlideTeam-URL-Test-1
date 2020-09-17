b877db677f94102cdae336f69579e224f8a703ee
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-09-17 16:00:03 +0530

    Update co_checkout.java

--- src/test/java/SetupClass/TestStep/co_checkout.java
+++ src/test/java/SetupClass/TestStep/co_checkout.java
@@ -221,7 +221,7 @@ public class co_checkout extends Set{
 		Thread.sleep(2000);
 		
 
-		 driver.findElement(By.cssSelector("ul.header > li:nth-child(1) > a:nth-child(1)")).click();
+		driver.findElement(By.xpath("//a[contains(.,'My Account')]")).click();
 		 Thread.sleep(3000);
 		 
 		
