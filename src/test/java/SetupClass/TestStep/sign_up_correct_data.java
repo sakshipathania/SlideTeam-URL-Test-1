4c0b6a8fc42e4a729f92fdbb3e8e912a08e94ec3
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-09-29 15:16:02 +0530

    Update sign_up_correct_data.java

--- src/test/java/SetupClass/TestStep/sign_up_correct_data.java
+++ src/test/java/SetupClass/TestStep/sign_up_correct_data.java
@@ -142,7 +142,7 @@ public class sign_up_correct_data extends Set {
 		
 		Thread.sleep(5000);
 		WebElement free_ppt_btn1=wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Free Stuff")));
-		action.moveToElement(Free Stuff).moveToElement(driver.findElement(By.xpath("//a[contains(text(),'Free Stuff')]"))).click().build().perform();
+		action.moveToElement(Free Stuff).moveToElement(driver.findElement(By.xpath("//a[contains(text(),'Free PPTs')]"))).click().build().perform();
 		Thread.sleep(7000);
 		//WebElement free_ppt_btn=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/header/div[1]/div[2]/div/nav/div/div/ul/li[2]/div/ul/li[1]/a")));
 	    //free_ppt_btn.click();
