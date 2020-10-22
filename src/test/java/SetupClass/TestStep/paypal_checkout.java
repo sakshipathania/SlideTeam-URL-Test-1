41ae4fa3d26831b2278b4c467eed38c8c7cfcbd0
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2020-10-22 17:39:36 +0530

    Update paypal_checkout.java

--- src/test/java/SetupClass/TestStep/paypal_checkout.java
+++ src/test/java/SetupClass/TestStep/paypal_checkout.java
@@ -150,12 +150,12 @@ public class paypal_checkout extends Set {
 		WebElement old_email = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='email']")));
 		js.executeScript("arguments[0].scrollIntoView();",old_email);
 		Thread.sleep(1000);
-	    old_email.sendKeys("himanshi@test.com");
+	    old_email.sendKeys("Qwerty120@gmail.com");
 	    Thread.sleep(1000);
 	    WebElement old_pass = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='pass']")));
 		js.executeScript("arguments[0].scrollIntoView();",old_pass);
 	    Thread.sleep(4000);
-	    old_pass.sendKeys("123456");
+	    old_pass.sendKeys("Qwerty@1");
 	    Thread.sleep(3000);
 	     WebElement old_login_btn=wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.login > span:nth-child(1)")));
 		 Thread.sleep(2000);
