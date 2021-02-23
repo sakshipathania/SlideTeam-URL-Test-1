955858dbf23c2d8d4801c4d533aacbe877c77a5b
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-02-23 15:58:41 +0530

    Update co_checkout.java

--- src/test/java/SetupClass/TestStep/co_checkout.java
+++ src/test/java/SetupClass/TestStep/co_checkout.java
@@ -746,7 +746,7 @@ public class co_checkout extends Set{
 		Thread.sleep(2000);
 		
 
-		driver.findElement(By.xpath("//a[contains(.,'My Account')]")).click();
+		/*driver.findElement(By.xpath("//a[contains(.,'My Account')]")).click();
 		 Thread.sleep(3000);
 		 
 		
@@ -793,7 +793,18 @@ try {
 		js.executeScript("arguments[0].scrollIntoView();",delete_profile);
 		delete_profile.click();
 		 Thread.sleep(3000);
-	}
+	}*/
+		try {
+			WebElement logout = driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]"));
+			if (logout.isEnabled()) {
+				logout.click();
+				Thread.sleep(8000);
+				driver.navigate().refresh();
+				Thread.sleep(2000);
+			}
+		} catch (NoSuchElementException Ext) {
+
+		}
 
 
 	
