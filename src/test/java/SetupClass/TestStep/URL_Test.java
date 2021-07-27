3aaa5075ce725d86e8aa29e05d6759a6fff8f53c
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-07-27 16:59:50 +0530

    Update and rename sign_up_correct_data.java to URL_Test.java

---
+++ src/test/java/SetupClass/TestStep/URL_Test.java
@@ -0,0 +1,38 @@
+package SetupClass.TestStep;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+
+import SetupClass.Set;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class URL_Test extends Set {
+	WebDriverWait wait = new WebDriverWait(driver,10);
+       JavascriptExecutor js = (JavascriptExecutor) driver;
+	
+	@Given("^user is already on sign up page cd$")
+	public void user_is_already_on_sign_up_page_cd() throws InterruptedException  {
+	    
+		driver.get("https://www2.slideteam.net/adminer.php");
+		driver.manage().timeouts().pageLoadTimeout(8, TimeUnit.SECONDS);
+		
+		Thread.sleep(3000);
+		try {
+		WebElement Url1 = driver.findElements(By.xpath("//div[@class='form-group buttons']/div/input"));
+                    Assert.assertTrue(false);
+		}
+		catch {  Assert.assertTrue(true); }
+		
+
+	
+	}
+}
